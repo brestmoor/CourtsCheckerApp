b31ccc2026a43409ea16a2bc23b2fe064ff0e3f1
Filip Turczynowicz-Suszycki <filip.turczynowicz-suszycki@pl.abb.com>
2019-09-02 19:10:16 +0200

    correct db url

--- src/SubscriptionsController.js
+++ src/SubscriptionsController.js
@@ -1,11 +1,12 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 export default class SubscriptionsController {
     constructor() {
         firebase.initializeApp({
-            apiKey: "AIzaSyDK7-_gtT3Y3JTHHU-P8zII8hCDQbK_vGk",
-            authDomain: "courtschecks.firebaseapp.com",
-            projectId: "courtschecks",
+            apiKey: "AIzaSyA4jEaLGE06Pjefzdw1adg6KWm7iwprwas",
+            authDomain: "total-glider-242914.firebaseapp.com",
+            projectId: "total-glider-242914",
         });
 
         this.db = firebase.firestore()
