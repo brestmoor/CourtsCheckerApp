fd862ac86c554b0461cd2068df867b6208bc7948
Filip Turczynowicz-Suszycki <filip.turczynowicz-suszycki@pl.abb.com>
2019-12-08 14:08:28 +0100

    add period subscriptions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
+import Form from 'react-bootstrap/Form'
 import DatePicker from "react-datepicker/es";
 import * as subscribing from "./subscribing.js";
 import "react-datepicker/dist/react-datepicker.css";
@@ -14,6 +15,7 @@ import getSubscriptionsController from "./SubscriptionsController";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import {convertToCET, getDateWithHour, getDayDateFor} from "./dateUtils";
+import moment from "moment";
 
 
 const App = () => {
@@ -23,20 +25,42 @@ const App = () => {
     const [fromTime, setFromTime] = useState(getDateWithHour(new Date(), 17));
     const [toTime, setToTime] = useState(getDateWithHour(new Date(), 19));
     const [subscriptions, setSubscriptions] = useState([]);
+    const [anyPeriod, setAnyPeriod] = useState(false);
+    const [period, setPeriod] = useState(1);
 
     const {addToast} = useToasts();
+
+    const handleSubscribing = (webSocketSubscription) => {
+        if (anyPeriod === false) {
+            saveSubscription(date, fromTime, toTime, webSocketSubscription);
+        } else {
+            debugger;
+            let fromTimeOfPeriod = fromTime;
+            let toTimeOfPeriod = moment(fromTime).add(period * 30, 'm').toDate();
+            while (toTimeOfPeriod <= toTime) {
+                saveSubscription(date, fromTimeOfPeriod, toTimeOfPeriod, webSocketSubscription);
+                fromTimeOfPeriod = moment(fromTimeOfPeriod).add(30, 'm').toDate();
+                toTimeOfPeriod = moment(toTimeOfPeriod).add(30, 'm').toDate();
+            }
+
+        }
+    };
+
+    const saveSubscription = (date, fromTime, toTime, webSocketSubscription) => {
+        subscriptionsController.save({
+            date: date,
+            fromTime: {hour: fromTime.getHours(), minute: fromTime.getMinutes()},
+            toTime: {hour: toTime.getHours(), minute: toTime.getMinutes()},
+            fromTimeDate: convertToCET(getDateWithHour(date, fromTime.getHours(), fromTime.getMinutes())),
+            expired: false,
+            subscription: JSON.parse(JSON.stringify(webSocketSubscription))
+        })
+    };
 
     const subscribe = () => {
         return subscribing.askPermission()
             .then(subscribing.subscribeUserToPush)
-            .then(subscription => subscriptionsController.save({
-                date: date,
-                fromTime: {hour: fromTime.getHours(), minute: fromTime.getMinutes()},
-                toTime: {hour: toTime.getHours(), minute: toTime.getMinutes()},
-                fromTimeDate: convertToCET(getDateWithHour(date, fromTime.getHours(), fromTime.getMinutes())),
-                expired: false,
-                subscription: JSON.parse(JSON.stringify(subscription))
-            }))
+            .then(handleSubscribing)
             .catch(console.log)
     };
 
@@ -65,7 +89,8 @@ const App = () => {
         computeStyle: {gpuAcceleration: false}
     };
 
-    const onSubscribeClick = () => subscribe()
+    const onSubscribeClick = () =>
+        subscribe()
         .then(updateActiveSubscriptions)
         .then(() => addToast('You will get notified, as soon as a court is free', {
             appearance: 'success',
@@ -80,7 +105,7 @@ const App = () => {
             <Row>
                 <Col>
                     <Card className="button-container"><Row>
-                        <Col lg={3} className="datepicker-container">
+                        <Col lg={2} className="datepicker-container">
                             <span className="datepicker-label">Date: </span>
                             <div className='datepicker-date'><DatePicker
                                 selected={date}
@@ -91,7 +116,7 @@ const App = () => {
                                 locale={en}
                             /></div>
                         </Col>
-                        <Col xs={6} lg={3} className="datepicker-container">
+                        <Col xs={6} lg={2} className="datepicker-container">
                             <span className="datepicker-label">From: </span>
                             <DatePicker
                                 selected={fromTime}
@@ -106,7 +131,7 @@ const App = () => {
                                 popperModifiers={popperModifiers}
                             />
                         </Col>
-                        <Col xs={6} lg={3} className="datepicker-container justify-content-right">
+                        <Col xs={6} lg={2} className="datepicker-container justify-content-right">
                             <span className="datepicker-label">To: </span>
                             <DatePicker
                                 selected={toTime}
@@ -120,6 +145,19 @@ const App = () => {
                                 className="form-control datepicker"
                                 popperModifiers={popperModifiers}
                             />
+                        </Col>
+                        <Col xs={6} lg={3} className="datepicker-container justify-content-right">
+                            <Form.Check value={anyPeriod} onChange={(e) => setAnyPeriod(e.target.checked)}/>
+                            <span className="datepicker-label">Period:</span>
+                            <Form.Control as="select" disabled={!anyPeriod} onChange={e => {console.log(e.target.value); setPeriod(e.target.value)}}>
+                                <option value={1}>0,5 h</option>
+                                <option value={2}>1 h</option>
+                                <option value={3}>1,5 h</option>
+                                <option value={4}>2 h</option>
+                                <option value={5}>2,5 h</option>
+                                <option value={6}>3,0 h</option>
+                                <option value={7}>3,5 h</option>
+                            </Form.Control>
                         </Col>
                         <Col lg={3} className="datepicker-container">
                             <div className='subscribe-button-container'>
